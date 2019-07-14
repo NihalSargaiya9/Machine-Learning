8be9e208b7a04d68e967d73c4fb893e85095789c
Nihal <nihalsargaiya4@gmail.com>
2019-07-14 20:41:39 +0530

    Logistic Regretion

---
+++ Python/Linear Regretion With One Variable/linear regretion.py
@@ -0,0 +1,113 @@
+import csv
+import matplotlib.pyplot as plt
+
+areas = []
+prices = []
+
+# Reading Training set
+
+with open('training set.csv','rt')as f:
+  data = csv.reader(f)
+  next(data)            # skip header
+  for x in data:
+  	areas.append(float(x[0]))
+  	prices.append(float(x[1]))
+
+# Mean Normalizing
+
+meanAreas = sum(areas)/len(areas)
+rArea = max(areas)-min(areas)
+areas = [((area- meanAreas)/rArea) for area in areas]
+
+
+# Gradient Decent
+
+def gradientDecent(m,c):
+	it=0
+	sameIt=0
+	while True:
+		temp0=0
+		temp1=0
+		for x,y in zip(areas,prices):
+			temp0+=((x*m)+c-y)
+			temp1+=((x*m)+c-y)*x
+		mlast = m
+		clast = c
+		m=m-(learn*(temp1/len(areas)))
+		c=c-(learn*(temp0/len(prices)))
+		costFunction(m,c)
+		
+		if(it>6000):
+			break
+		it+=1
+	return m,c
+
+squareMeanErrors=[]
+
+
+# Linear Regretion function to find Sqare Mean Errors
+
+def costFunction(m,c):
+	squareMeanErrorsTemp=[]
+	for x,y in zip(areas,prices):
+		temp = (((x*m)+c)-y)**2
+		squareMeanErrorsTemp.append(temp)
+	squareMeanErrors.append(sum(squareMeanErrorsTemp)/(len(squareMeanErrorsTemp)*2))
+
+
+# Prediction of Values
+
+def prdict(val,m,c):
+	val = (val- meanAreas)/rArea		# Mean Normalize The value (val = User Input)
+	temp = m*val+c 						# Calculate Y cordinates
+	if round(temp,0)<0:
+		print("Please Enter larger Value")
+		return
+	print("Price in Rupees(RS) is :",round(temp,0))							# Print Answer
+
+
+
+
+fig, grph = plt.subplots(nrows=1, ncols=2) # dividing plot into two sub graphs
+grph[0].scatter(areas,prices,s=10) 		   # Plotting points on graph 0
+
+m =1										# Slope (Can be any value)
+c = 2										# Intercept (Can be any value)
+
+learn = 0.007								# Learning Rate
+
+print("Learning ... (Please wait)")
+
+m,c=gradientDecent(m,c)
+
+
+pointsX = areas 							# X codinates for potting Line
+pointsY = [((m*t)+c) for t in pointsX]		# Y cordiantes for Plotting Line
+
+grph[0].plot(pointsX,pointsY,c="red")		# Plot Line 
+
+grph[1].plot(squareMeanErrors)				#plot Square mean error for Debuging Alogorithm
+
+
+# Setting up all Labels 
+
+grph[0].set_xlabel('Size in ($ft^2$)')
+grph[0].set_ylabel('Price in rupees')
+grph[0].set_title("Flats in vijaynagar")
+grph[1].set_ylabel('Square mean Error')
+grph[1].set_xlabel('itterations')
+
+
+# Take Inputs For Predictions
+
+noTest = int(input("Please enter Number of Test Cases : ")) # No of test cases
+
+for _ in range(noTest):
+	test = float(input("Enter Area in (Sqare ft) : ")) #Test Case
+	prdict(test,m,c)	 #Prediction
+
+
+
+
+
+plt.show()
